3bf43f842e652557ecfd626e962407e4993b059b
SyedHassan1212 <nh31097@gmail.com>
2025-08-09 01:48:16 +0500

    added express for handeling

---
+++ backend/index.js
@@ -0,0 +1,12 @@
+ const express = require('express');
+const app = express();
+app.use (express.json())
+app.post('/todos' , function(req,res){
+
+})
+app.get('/todos' , function(req,res){
+    
+})
+
+
+app.listen(3000)
