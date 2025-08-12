bb6f75ded539cad88ad8faf4a7ccaeace6ae34d5
SyedHassan1212 <nh31097@gmail.com>
2025-08-12 14:48:42 +0500

    implement creation of todos te todos via react

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,8 +6,8 @@ function App() {
   
 const [todos , setTodos] = useState([]);
 fetch("http://localhost:3000/todos")
-.then(async function(){
-  const json = await resizeBy.json();
+.then(async function(res){
+  const json = await res.json();
 setTodos(json.todos)
 })
   return (
--- frontend/src/components/CreateTodo.jsx
+++ frontend/src/components/CreateTodo.jsx
@@ -1,8 +1,23 @@
 export function CreateTodo(){
+    const 
     return <div>
         <input type =" text" placeholder="title"></input>
         <input type =" text" placeholder="description"></input>
-        <button>add todo</button>
+        <button 
+        onClick={()=> {
+            fetch("http://localhost:3000/todo"),{
+                method : "POST",
+                body: {
+
+                }
+            }
+            .then(async function(res) {
+                const res = await res.json();
+                alert("Todo added")
+            })
+        }}
+        
+        >add todo</button>
         
     </div>
 } 
